a70cdafbd78d8778779b3c7c5bd28a5f4fbc4a5a
mynameisrobot <34392108+mynameisrobot@users.noreply.github.com>
2019-11-25 14:16:48 +0800

    Update url.py

--- prework/willdoneproject/url.py
+++ prework/willdoneproject/url.py
@@ -4,5 +4,7 @@ urlpatterns=[
     url('gettask/',listproject.as_view()),
     url('add/',addproject.as_view()),
     url('updata/',updataproject.as_view()),
-    url('delete/',deleteproject.as_view())
+    url('delete/',deleteproject.as_view()),
+     url('moedl/(?P<pk>\w+)',modelview.as_view({'post':'create','put':'update','delete':'destroy'}))
+    url('moedl/',modelview.as_view({'get':'retrieveall'}))
 ]
